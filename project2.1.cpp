bcdfd934e94a99b566222593683145a59558f14b
llsswypbb <lls_@tju.edu.cn>
2017-03-27 10:50:24 +0800

    ddd

---
+++ project2.1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cfloat>
+using namespace std;
+
+int main()
+{
+     float y1 = FLT_MIN;
+     cout << y1 * y1 << endl;
+
+     float y2 = FLT_MAX;
+     cout << y2 * y2 << endl;
+
+     float y3 = 1.0;
+     cout << y3 * y3 << endl;
+
+    return 0;
+}
